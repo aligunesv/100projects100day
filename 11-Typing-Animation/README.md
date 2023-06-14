3f62a838aee6704b6907e265c6b9d4c943b08a43
Ali Güneş <82121296+aligunesv@users.noreply.github.com>
2023-06-14 21:13:09 +0300

    Adding typing animation with html css and js

---
+++ 11-Typing-Animation/README.md
@@ -0,0 +1,7 @@
+# Simple Typing Effect with HTML, CSS and JAVASCRIPT
+
+It's a simple typing effect with html css and javascript
+
+----
+
+You can visit my website [imvora.dev](https://www.imvora.dev)
---
+++ 11-Typing-Animation/script.js
@@ -0,0 +1,14 @@
+var string = "Hey mom! I code simple typing effect!"; 
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',80); 
+
+}
+frameLooper();
